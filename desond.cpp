d6cdbb5fa5958828e0d67a0c8bbf9ea4ed2aac09
moushuai1020 <1690116718@qq.com>
2019-11-30 21:57:42 +0800

    fisrt commit

---
+++ desond.cpp
@@ -0,0 +1,23 @@
+#include<iostream>//利用梯度下降法求y=x^2的极小值点
+using namespace std;
+int main()
+{
+    float x,dx,step;
+    int i;
+    step=0.2;
+    x=10;
+
+    for(i=1;i<=50;i++)
+    {
+        dx=2*x;
+        x=x-step*dx;
+        if(i==10)cout<<"迭代是10步结果为："<<x<<endl;
+        if(i==20)cout<<"迭代是20步结果为："<<x<<endl;
+        if(i==30)cout<<"迭代是30步结果为："<<x<<endl;
+        if(i==40)cout<<"迭代是40步结果为："<<x<<endl;
+        if(i==50)cout<<"迭代是50步结果为："<<x<<endl;
+
+    }
+
+    return 0;
+}
